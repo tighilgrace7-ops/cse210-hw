ba096bf0344643798e5241613bb1861ef96f3120
Grace Tighil <tighilgrace7@gmail.com>
2026-05-13 21:16:12 +0100

    update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign ="";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+
+        if (lastDigit>= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign ="-";
+        }
+
+        if (letter =="A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is {letter}{sign}");
     }
 }
