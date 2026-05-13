776419ae0bb123dfea492247397241a676cf455f
Grace Tighil <tighilgrace7@gmail.com>
2026-05-13 20:16:49 +0100

    update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is last name?");
+        string last = Console.ReadLine();
+
+        Console.Write($"Your name is {last}, {first} {last}.");
     }
 }
