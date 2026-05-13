3d1af92ec66c88e621d9382b41d4057e24ebeae3
Grace Tighil <tighilgrace7@gmail.com>
2026-05-13 21:53:28 +0100

    update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        //Display this once
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Loop for numbers
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+
+            userNumber = int.Parse(Console.ReadLine()); 
+
+            // Do not add 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Find the sum
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Find the average
+        float average = (float)sum / numbers.Count;
+
+        Console. WriteLine($"The average is: {average}");
+
+        // Find the largest number
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
